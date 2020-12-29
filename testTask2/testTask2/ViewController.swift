15e906a67e9463ec25d5e6c848d9f56d86adabef
TonyJem <a.jemanov@gmail.com>
2020-12-29 22:23:06 +0200

    override func loadView()

--- testTask2/testTask2/ViewController.swift
+++ testTask2/testTask2/ViewController.swift
@@ -1,7 +1,17 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet private weak var someLabel: UILabel!
+    
+    override func loadView() {
+        print(#function)
+        let view = UIView()
+        self.view = view
+        self.view.backgroundColor = .blue
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
