feaef91d5c91fc78b7b7e306b300c6ed99d278b4
TonyJem <a.jemanov@gmail.com>
2020-12-29 23:54:01 +0200

    Add functionality to SecondViewController

--- testTask2/testTask2/SecondViewController.swift
+++ testTask2/testTask2/SecondViewController.swift
@@ -1,29 +1,39 @@
-//
-//  SecondViewController.swift
-//  testTask2
-//
-//  Created by Office-iMac on 2020-12-29.
-//
-
 import UIKit
 
 class SecondViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        print("SecondVC", #function)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("SecondVC", #function)
     }
-    */
-
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print("SecondVC", #function)
+    }
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        print("SecondVC", #function)
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        print("SecondVC", #function)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        print("SecondVC", #function)
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        print("SecondVC", #function)
+    }
 }
